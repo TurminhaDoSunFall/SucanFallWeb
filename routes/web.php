1ed15c10e6d75d7c0c93134549452c47461cb3f4
Isis <emailgenerico742@gmail.com>
2021-09-12 18:05:38 -0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('inicial', ["nome" => "Isis"]);
 });
+
+Route::get("/U", [UsuarioController::class , "index"]);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
