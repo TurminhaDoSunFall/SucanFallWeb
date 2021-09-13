47f1bd9969b637645642c000a6098a0b674d7b8a
joaomarcoshess <75141271+joaomarcoshess@users.noreply.github.com>
2021-09-13 14:02:13 -0300

    Creditos

---
+++ resources/views/creditos.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Chango&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Chango" rel="stylesheet">
+    <style>
+        body {
+          font-family: "Chango", sans-serif;
+        }
+        </style>
+    <title>Créditos</title>
+</head>
+<body>
+    <header class="col-12">
+        <div class="menu col-2">
+            <img src="assets/logo.png" class="image">
+        </div>
+        <div class="menu col-10">
+            <p>CRÉDITOS</p>
+        </div>
+    </header>  
+    <section>
+        <p>Isis S. P.</p>
+        <p>João Marcos S. H.</p>
+        <p>Pedro Luis C.</p><br>
+        <p>----------------</p>
+        <p>CEFET-MG</p>
+        <p>2021</p><br><br>
+        <p>Agradecimentos</p>
+        <p>Lazin</p>
+        <p>O Sol</p>
+        <p>O galo do vizinho do Pedro</p>
+        <p>Nossas mães</p>
+        <p>E os pais também</p>
+        <p>(Pra não ter ciúme)</p>
+        <img src="assets/logo.png">
+    </section>
+</body>
+</html>
