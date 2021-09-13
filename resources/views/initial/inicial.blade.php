61b6554d3bd639ac96c5af4a508702ee577b7623
Isis <emailgenerico742@gmail.com>
2021-09-13 18:59:51 -0300

    isis  - processo carrossel aula 09 - 13-9

---
+++ resources/views/initial/inicial.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <
+    <link href="https://fonts.googleapis.com/css2?family=Chango" rel="stylesheet">
+    <style>
+        body {
+          font-family: "Chango", sans-serif;
+        }
+        </style>
+    <title>Minha Galeria</title>
+</head>
+<body>
+  <div class ="row">
+   <header class="col-12">
+      <div class="menu col-2">
+          <img src="assets/logo.png" class="image" style="max-width:60%;height:auto;">
+      </div>
+      <div class="menu col-10">
+          <p style="font-size:3vw">MINHA GALERIA</p>
+      </div>
+   </header>
+</head>
+    <h1>{{$nome}}</h1>
+    <a href="/">Inicial</a>
+</body>
+</html>
