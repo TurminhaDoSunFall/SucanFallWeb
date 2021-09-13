df1e1a0a18ddefd905d7c64d3b43f6fd4e6a11e2
joaomarcoshess <75141271+joaomarcoshess@users.noreply.github.com>
2021-09-13 14:04:54 -0300

    implementou sem misericórdia o arquivo config...

---
+++ resources/views/config.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Chango" rel="stylesheet">
+    <style>
+        body {
+          font-family: "Chango", sans-serif;
+        }
+        </style>
+    <link rel="stylesheet" href="css/config.css">
+    <title>Configurações</title>
+</head>
+<body>
+    <header class="col-12">
+        <div class="menu col-2">
+            <img src="assets/logo.png" class="image">
+        </div>
+        <div class="menu col-10">
+            <p>CONFIGURAÇÕES</p>
+        </div>
+    </header>  
+    <nav>
+        <div class="alo col-12">
+            <label for="viverfeliz">Viver Feliz:</label>
+            <input type="checkbox" id="viverfeliz" name="whatever" value="Felicidade"><br><br>
+            <label for="som">Desativar som:</label>
+            <input type="checkbox" id="som" name="Somzin" value="desalegria"><br><br>
+        </div>
+        <div class="botoes col-12">
+            <li class="botao">
+                <a href="login.html">Deletar Perfil</a><br>
+                <a href="index.html">Log Out</a>
+            </li>
+        </div>
+    </nav>
+</body>
+</html>
