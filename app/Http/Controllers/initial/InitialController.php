029af6f6f32e481732bbed9b8f4afff5acc76424
Isis <emailgenerico742@gmail.com>
2021-09-15 21:44:42 -0300

    pg inicial "feita" - 15-9

--- app/Http/Controllers/initial/InitialController.php
+++ app/Http/Controllers/initial/InitialController.php
@@ -10,13 +10,15 @@ class InitialController extends Controller
     public function index() {
         $carrossel = [
             "imagens" => [
-                "nome" => "imagemdeexemplo",
-                "url" => "img/carrossel/imagemdeexemplo.png"
-            ],
-            [ 
-                "nome" => "por_sol",
-                "url" => "img/carrossel/por_sol.jpg"
-            ]
+                    [
+                        "nome" => "imagemdeexemplo",
+                        "url" => "img/carrossel/imagemdeexemplo.png"
+                    ],
+                    [ 
+                        "nome" => "por_sol",
+                        "url" => "img/carrossel/por_sol.jpg"
+                    ]
+                ]
             ];
         return view("initial/initial", $carrossel);
     }
--- resources/views/initial/initial.blade.php
+++ resources/views/initial/initial.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" >
     <title>Bem-vindo</title>
     <link rel="stylesheet" href="{{asset('css/initial/initial.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/initial/carrossel.css')}}"/>
     <link href="https://fonts.googleapis.com/css2?family=Chango" rel="stylesheet">
     <style>
         body {
@@ -15,16 +16,43 @@
     <title>Minha Galeria</title>
 </head>
 <body>
+
   <div class ="row">
    <header class="col-12">
       <div class="menu col-2">
           <img src="{{asset('img/logo.png')}}" class="image" alt="Logo">
       </div>
-      <div class="menu col-10">
+      <div class="menu col-8">
           <p style="font-size:3vw">BEM VINDO</p>
       </div>
+      <div class="menu col-2">
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+      </div>
    </header>
-   <a href="/Historia">AAAAAAAAOOOOOOPPPAAAAAAAAAA</a>
+   <main>
+        <section class="carrossel">
+            <ol>
+                @foreach($imagens as $imagem)
+                <li>
+                    <img src="{{asset($imagem['url'])}}" alt="{{$imagem['nome']}}" />
+                </li>
+                @endforeach
+            </ol>
+        </section>
+        <h1>Os pôres dos sóis mais belos que podemos vivenciar</h1>
+   </main>
 </head>
 </body>
 </html>
