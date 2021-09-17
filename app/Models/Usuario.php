e3d550a8784111162f1822415b05d96f895a55d5
Isis <emailgenerico742@gmail.com>
2021-09-16 21:59:41 -0300

    tabela bd users - usuario = 16-9

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     use HasFactory;
+    
+    
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
+
+
